481e1d308aa7d481a865adc15a5f29450b6098a3
Azat <azat4dev@gmail.com>
2023-11-23 10:14:42 +0400

    Refactor - Move AdjustDateToStartDateInBacklogImpl in a separate file

---
+++ server/daily-tasks/src/main/java/com/azatdev/dailytasks/domain/usecases/utils/AdjustDateToStartDateInBacklogImpl.java
@@ -0,0 +1,29 @@
+package com.azatdev.dailytasks.domain.usecases.utils;
+
+import java.time.LocalDate;
+
+import com.azatdev.dailytasks.domain.models.Backlog;
+
+
+public class AdjustDateToStartDateInBacklogImpl implements AdjustDateToStartOfBacklog {
+
+    private LocalDate adjustToMonday(LocalDate date) {
+
+        var dayOfWeek = date.getDayOfWeek().getValue();
+        var daysToAdjust = dayOfWeek - 1;
+        return date.minusDays(daysToAdjust);
+    }
+
+    @Override
+    public LocalDate calculate(LocalDate date, Backlog.Duration duration) {
+
+        switch (duration) {
+            case DAY:
+                return date;
+            case WEEK:
+                return adjustToMonday(date);
+        }
+
+        return date;
+    }
+}
--- server/daily-tasks/src/test/java/com/azatdev/dailytasks/domain/usecases/utils/AdjustDateToStartDateInBacklogTests.java
+++ server/daily-tasks/src/test/java/com/azatdev/dailytasks/domain/usecases/utils/AdjustDateToStartDateInBacklogTests.java
@@ -11,28 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.azatdev.dailytasks.domain.models.Backlog;
 import com.azatdev.dailytasks.domain.usecases.utils.AdjustDateToStartOfBacklog;
 
-class AdjustDateToStartDateInBacklogImpl implements AdjustDateToStartOfBacklog {
-
-    private LocalDate adjustToMonday(LocalDate date) {
-
-        var dayOfWeek = date.getDayOfWeek().getValue();
-        var daysToAdjust = dayOfWeek - 1;
-        return date.minusDays(daysToAdjust);
-    }
-
-    @Override
-    public LocalDate calculate(LocalDate date, Backlog.Duration duration) {
-
-        switch (duration) {
-            case DAY:
-                return date;
-            case WEEK:
-                return adjustToMonday(date);
-        }
-
-        return date;
-    }
-}
 
 class AdjustDateToStartDateInBacklogTests {
 
