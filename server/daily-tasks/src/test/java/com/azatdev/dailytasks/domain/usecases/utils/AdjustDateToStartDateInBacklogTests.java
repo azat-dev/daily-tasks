4942f2ec61d9e28f1ab72fa3f2fece3057c51afc
Azat <azat4dev@gmail.com>
2023-11-23 10:12:30 +0400

    Implement AdjustDateToStartDateInBacklogTests

--- server/daily-tasks/src/test/java/com/azatdev/dailytasks/domain/usecases/utils/AdjustDateToStartDateInBacklogTests.java
+++ server/daily-tasks/src/test/java/com/azatdev/dailytasks/domain/usecases/utils/AdjustDateToStartDateInBacklogTests.java
@@ -13,13 +13,21 @@ import com.azatdev.dailytasks.domain.usecases.utils.AdjustDateToStartOfBacklog;
 
 class AdjustDateToStartDateInBacklogImpl implements AdjustDateToStartOfBacklog {
 
+    private LocalDate adjustToMonday(LocalDate date) {
+
+        var dayOfWeek = date.getDayOfWeek().getValue();
+        var daysToAdjust = dayOfWeek - 1;
+        return date.minusDays(daysToAdjust);
+    }
+
     @Override
     public LocalDate calculate(LocalDate date, Backlog.Duration duration) {
+
         switch (duration) {
             case DAY:
                 return date;
             case WEEK:
-                return date;
+                return adjustToMonday(date);
         }
 
         return date;
