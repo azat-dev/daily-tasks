31789cebdb0096292c63a94f80b145cfbd6245e5
Azat <azat4dev@gmail.com>
2023-11-27 20:35:51 +0400

    Clean the code

--- server/daily-tasks/src/test/java/com/azatdev/dailytasks/data/repositories/persistence/jpa/JPATasksRepositoryTests.java
+++ server/daily-tasks/src/test/java/com/azatdev/dailytasks/data/repositories/persistence/jpa/JPATasksRepositoryTests.java
@@ -2,7 +2,6 @@ package com.azatdev.dailytasks.data.repositories.persistence.jpa;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
