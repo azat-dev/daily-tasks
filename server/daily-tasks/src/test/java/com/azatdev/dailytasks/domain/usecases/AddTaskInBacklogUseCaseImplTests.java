ff76457c2110ba8590b6065feda11e93638930b6
Azat <azat4dev@gmail.com>
2023-11-28 23:36:58 +0400

    Test that will create a new task

--- server/daily-tasks/src/test/java/com/azatdev/dailytasks/domain/usecases/AddTaskInBacklogUseCaseImplTests.java
+++ server/daily-tasks/src/test/java/com/azatdev/dailytasks/domain/usecases/AddTaskInBacklogUseCaseImplTests.java
@@ -11,6 +11,8 @@ import com.azatdev.dailytasks.domain.models.Backlog;
 import com.azatdev.dailytasks.domain.models.Task;
 import com.azatdev.dailytasks.utils.Result;
 
+import jakarta.annotation.Nonnull;
+
 interface AddTaskInBacklogUseCase {
 
     enum Error {
@@ -20,6 +22,26 @@ interface AddTaskInBacklogUseCase {
     Result<Task, Error> execute(
         LocalDate date,
         Backlog.Duration backlogDuration
+    );
+}
+
+record NewTaskData(
+    @Nonnull String title,
+    Task.Priority priority,
+    @Nonnull String description
+) {
+}
+
+interface TasksRepositoryAdd {
+
+    enum Error {
+        INTERNAL_ERROR,
+        BACKLOG_NOT_FOUND
+    }
+
+    Result<Task, Error> add(
+        Long backlogId,
+        NewTaskData newTaskData
     );
 }
 
@@ -45,11 +67,14 @@ interface AddBacklogForDateIfDoesntExistUseCase {
 class AddTaskInBacklogUseCaseImpl implements AddTaskInBacklogUseCase {
 
     private final AddBacklogForDateIfDoesntExistUseCase addBacklogUseCase;
+    private final TasksRepositoryAdd tasksRepository;
 
     public AddTaskInBacklogUseCaseImpl(
-        AddBacklogForDateIfDoesntExistUseCase addBacklogUseCase
+        AddBacklogForDateIfDoesntExistUseCase addBacklogUseCase,
+        TasksRepositoryAdd tasksRepository
     ) {
         this.addBacklogUseCase = addBacklogUseCase;
+        this.tasksRepository = tasksRepository;
     }
 
     @Override
@@ -77,21 +102,28 @@ class AddTaskInBacklogUseCaseImplTests {
 
     private record SUT(
         AddTaskInBacklogUseCase useCase,
-        AddBacklogForDateIfDoesntExistUseCase addBacklogUseCase
+        AddBacklogForDateIfDoesntExistUseCase addBacklogUseCase,
+        TasksRepositoryAdd tasksRepository
     ) {
     }
 
     private SUT createSUT() {
         final var addBacklogUseCase = mock(AddBacklogForDateIfDoesntExistUseCase.class);
 
+        final var tasksRepository = mock(TasksRepositoryAdd.class);
+
         return new SUT(
-            new AddTaskInBacklogUseCaseImpl(addBacklogUseCase),
-            addBacklogUseCase
+            new AddTaskInBacklogUseCaseImpl(
+                addBacklogUseCase,
+                tasksRepository
+            ),
+            addBacklogUseCase,
+            tasksRepository
         );
     }
 
     @Test
-    void executeShouldAddBacklogAndReturnTaskTest() {
+    void executeShouldAddBacklogAddTaskAndReturnCreatedTaskTest() {
 
         // Given
         final var backlogDuration = Backlog.Duration.DAY;
@@ -99,10 +131,29 @@ class AddTaskInBacklogUseCaseImplTests {
 
         final var backlogId = 1L;
 
+        final var newTaskData = new NewTaskData(
+            "title",
+            Task.Priority.HIGH,
+            "description"
+        );
+
+        final var expectedTask = TestDomainDataGenerator.anyTask(1L);
+
         final var sut = createSUT();
 
-        given(sut.addBacklogUseCase.execute(backlogStartDate, backlogDuration))
-            .willReturn(Result.success(backlogId));
+        given(
+            sut.addBacklogUseCase.execute(
+                backlogStartDate,
+                backlogDuration
+            )
+        ).willReturn(Result.success(backlogId));
+
+        given(
+            sut.tasksRepository.add(
+                backlogId,
+                newTaskData
+            )
+        ).willReturn(Result.success(expectedTask));
 
         // When
         final var result = sut.useCase.execute(
@@ -111,13 +162,19 @@ class AddTaskInBacklogUseCaseImplTests {
         );
 
         // Then
-        
         then(sut.addBacklogUseCase).should(times(1))
             .execute(
                 backlogStartDate,
                 backlogDuration
             );
 
+        then(sut.tasksRepository).should(times(1))
+            .add(
+                backlogId,
+                newTaskData
+            );
+
         assertThat(result).isNotNull();
+        assertThat(result.getValue()).isEqualTo(expectedTask);
     }
 }
