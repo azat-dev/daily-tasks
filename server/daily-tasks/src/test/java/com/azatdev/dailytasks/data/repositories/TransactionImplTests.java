c769b71abb92536bac98d766f061f11c619f6dfb
Azat <azat4dev@gmail.com>
2023-11-29 21:48:24 +0400

    Refactor the code

--- server/daily-tasks/src/test/java/com/azatdev/dailytasks/data/repositories/TransactionImplTests.java
+++ server/daily-tasks/src/test/java/com/azatdev/dailytasks/data/repositories/TransactionImplTests.java
@@ -13,47 +13,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import com.azatdev.dailytasks.data.repositories.data.TransactionImpl;
 import com.azatdev.dailytasks.domain.interfaces.repositories.transaction.Transaction;
-
-import jakarta.annotation.Nonnull;
-
-class TransactionImpl implements Transaction {
-
-    private final PlatformTransactionManager transactionManager;
-
-    private TransactionStatus currentTransactionStatus;
-
-    public TransactionImpl(@Nonnull PlatformTransactionManager transactionManager) {
-        this.transactionManager = transactionManager;
-    }
-
-    @Override
-    public void begin() {
-        final var definition = new DefaultTransactionDefinition();
-        this.currentTransactionStatus = this.transactionManager.getTransaction(definition);
-    }
-
-    @Override
-    public void commit() {
-
-        if (this.currentTransactionStatus == null) {
-            throw new IllegalStateException("Transaction not started");
-        }
-
-        this.transactionManager.commit(this.currentTransactionStatus);
-
-    }
-
-    @Override
-    public void rollback() {
-
-        if (this.currentTransactionStatus == null) {
-            throw new IllegalStateException("Transaction not started");
-        }
-
-        this.transactionManager.rollback(this.currentTransactionStatus);
-    }
-}
 
 class TransactionImplTests {
 
