edae8e5ac6f7d62351469df370a1efbc79f66c48
Azat <azat4dev@gmail.com>
2023-12-01 11:46:07 +0400

    Fix UserData

--- server/daily-tasks/src/main/java/com/azatdev/dailytasks/data/repositories/data/user/UserData.java
+++ server/daily-tasks/src/main/java/com/azatdev/dailytasks/data/repositories/data/user/UserData.java
@@ -3,10 +3,12 @@ package com.azatdev.dailytasks.data.repositories.data.user;
 import java.util.UUID;
 
 import jakarta.annotation.Nonnull;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
+@Entity
 @Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "username") })
 public record UserData(
     @Id UUID id,
