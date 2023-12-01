25750873167ad8128d4f16572591cfbfce5fde6b
Azat <azat4dev@gmail.com>
2023-12-01 23:55:27 +0400

    Implement JWTServiceImpl

--- server/daily-tasks/src/test/java/com/azatdev/dailytasks/presentation/security/services/JWTServiceTests.java
+++ server/daily-tasks/src/test/java/com/azatdev/dailytasks/presentation/security/services/JWTServiceTests.java
@@ -1,34 +1,33 @@
 package com.azatdev.dailytasks.presentation.security.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Date;
 import java.util.UUID;
+
+import javax.crypto.SecretKey;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Date;
-
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 
 class JWTServiceImpl implements JWTService {
 
-
-    private String jwtSecret;
+    private SecretKey secretKey;
     private int jwtExpirationInMs;
 
     public JWTServiceImpl(
         @Value("${app.security.jwtSecret}") String jwtSecret,
         @Value("${app.security.jwtExpirationInMs}") int jwtExpirationInMs
     ) {
-        this.jwtSecret = jwtSecret;
+        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
         this.jwtExpirationInMs = jwtExpirationInMs;
     }
 
@@ -36,50 +35,78 @@ class JWTServiceImpl implements JWTService {
     public String generateToken(UUID userId) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
-        final var key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
 
         return Jwts.builder()
-                .subject(userId.toString())
-                .issuedAt(new Date())
-                .expiration(expiryDate)
-                .signWith(key, Jwts.SIG.HS512)
-                .compact();
+            .subject(userId.toString())
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(
+                secretKey,
+                Jwts.SIG.HS512
+            )
+            .compact();
     }
 
     @Override
     public UUID getUserIdFromToken(String token) {
-        // TODO Auto-generated method stub
-        return null;
+
+        final var encodedUserId = Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .getSubject();
+
+        return UUID.fromString(encodedUserId);
     }
 
     @Override
     public boolean validateToken(String authToken) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validateToken'");
+        try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            // log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            // log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            // log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            // log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            // log.error("JWT claims string is empty.");
+        }
+        return false;
     }
 
 }
 
 class JWTServiceTests {
 
-    private final JWTService sut = new JWTServiceImpl("secretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecret", 10000);
+    private final JWTService sut = new JWTServiceImpl(
+        "secretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecret",
+        10000
+    );
 
     @Test
     void generateTokenMustReturnTokenTest() {
-    
+
         // Given
         final var userId = UUID.randomUUID();
-    
+
         // When
         final var result = sut.generateToken(userId);
-    
+
         // Then
         assertThat(result).isNotNull();
     }
 
     @Test
     void validateTokenMustFailOnWrongTokenTest() {
-    
+
         // Given
         final var wrongToken = "wrongToken";
 
@@ -92,7 +119,7 @@ class JWTServiceTests {
 
     @Test
     void validateTokenMustSuccedOnCorrectTokenTest() {
-    
+
         // Given
         final var token = sut.generateToken(UUID.randomUUID());
 
@@ -105,20 +132,22 @@ class JWTServiceTests {
 
     @Test
     void getUserIdFromTokenMustFailOnWrongToken() {
-    
+
         // Given
         final var wrongToken = "wrongToken";
 
         // When
-        final var userId = sut.getUserIdFromToken(wrongToken);
+        assertThrows(
+            Exception.class,
+            () -> sut.getUserIdFromToken(wrongToken)
+        );
 
         // Then
-        assertThat(userId).isNull();
     }
 
     @Test
     void getUserIdFromTokenMustReturnUserIdOnCorrectToken() {
-    
+
         // Given
         final var userId = UUID.randomUUID();
         final var token = sut.generateToken(userId);
