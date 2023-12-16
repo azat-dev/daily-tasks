b46096e7228cf12b3666aaeb619220256503e2ea
Azat <azat4dev@gmail.com>
2023-12-17 01:31:40 +0400

    Client: Update styles

--- client/src/presentation/pages/DayPage/Row/RowView.tsx
+++ client/src/presentation/pages/DayPage/Row/RowView.tsx
@@ -3,15 +3,17 @@ import RowViewModel from "./ViewModel/RowViewModel";
 import useUpdatesFrom from "../../LogInPage/useUpdatesFrom";
 import ActionButtonView from "../ActionButton/ActionButtonView";
 
+import styles from "./styles.module.scss";
+
 interface RowViewProps {
     viewModel: RowViewModel;
 }
 
 const RowView = ({ viewModel: vm }: RowViewProps) => {
-    useUpdatesFrom(vm.title, vm.status, vm.priority, vm.createdAt);
+    useUpdatesFrom(vm.title, vm.status, vm.priority, vm.createdAt, vm.isActive);
 
     return (
-        <tr>
+        <tr className={styles.row} data-active={!!vm.isActive.value}>
             <td>{vm.title.value}</td>
             <td>{vm.status.value}</td>
             <td>{vm.priority.value}</td>
