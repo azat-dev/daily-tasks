6a78e37de24e4b7ef458eafb2d5e941fbb4b3fdb
Azat <azat4dev@gmail.com>
2023-12-18 22:53:52 +0400

    Add DeleteTaskDaoImpl

---
+++ server/daily-tasks/src/main/java/com/azatdev/dailytasks/data/repositories/data/DeleteTaskDaoImpl.java
@@ -0,0 +1,24 @@
+package com.azatdev.dailytasks.data.repositories.data;
+
+import com.azatdev.dailytasks.data.repositories.persistence.jpa.JpaTasksRepository;
+import com.azatdev.dailytasks.domain.exceptions.TaskNotFoundException;
+import com.azatdev.dailytasks.domain.interfaces.dao.DeleteTaskDao;
+
+public final class DeleteTaskDaoImpl implements DeleteTaskDao {
+
+    private final JpaTasksRepository tasksRepository;
+
+    public DeleteTaskDaoImpl(JpaTasksRepository tasksRepository) {
+        this.tasksRepository = tasksRepository;
+    }
+
+    @Override
+    public void execute(long taskId) throws TaskNotFoundException {
+
+        final var task = tasksRepository.findById(taskId)
+            .orElseThrow(() -> new TaskNotFoundException(taskId));
+
+        tasksRepository.delete(task);
+        tasksRepository.flush();
+    }
+}
